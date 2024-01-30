ae9f2d8f12ae6a2880c5197e2b0cca74bda258a9
Daniel <154093698+Daniel-Cafe@users.noreply.github.com>
2024-01-30 11:25:28 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function showNav () {
+    var div = document.getElementById('nav-mobile');
+    div.style.display = 'block';
+}
+
+function closeNav () {
+    var div = document.getElementById('nav-mobile');
+    div.style.display = 'none';
+}
+
+function showOverlay () {
+    var div = document.getElementById('overlay');
+    div.style.display = 'block';
+}
+
+function closeOverlay () {
+    var div = document.getElementById('overlay');
+    div.style.display = 'none';
+}
